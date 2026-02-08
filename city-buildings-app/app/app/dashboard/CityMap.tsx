5d1c6ba6190db67ec017bb4b67b4f98c6c90a038
Chima Iheaturu <40758176+ciheaturu@users.noreply.github.com>
2026-02-08 19:40:02 +0100

    Move Leaflet icon fix into CityMap component to prevent SSR error

--- city-buildings-app/app/app/dashboard/CityMap.tsx
+++ city-buildings-app/app/app/dashboard/CityMap.tsx
@@ -4,6 +4,15 @@ import { useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import L from "leaflet";
 
+if (typeof window !== "undefined") {
+  delete (L.Icon.Default.prototype as any)._getIconUrl;
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+    iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+  });
+}
+
 type MapPoint = {
   id: string;
   building_name: string;
