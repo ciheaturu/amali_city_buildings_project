b1fca3a5a55e51b6438d3679859b1f05d6329f31
Chima Iheaturu <40758176+ciheaturu@users.noreply.github.com>
2026-02-09 22:23:40 +0100

    Add BuildingMap components and Leaflet dependencies

--- city-buildings-app/app/admin/dashboard/BuildingMap.tsx
+++ city-buildings-app/app/admin/dashboard/BuildingMap.tsx
@@ -4,7 +4,6 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-// Fix for default marker icons in Leaflet
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
